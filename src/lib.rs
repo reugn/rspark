be5b3be5290804d2d419467bda4c79664fb50ab6
reugn <reugpro@gmail.com>
2019-10-30 19:54:26 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+mod rspark {
+
+    const Ticks: [char; 8] = [
+        '\u{2581}', '\u{2582}', '\u{2583}', '\u{2584}', '\u{2585}', '\u{2586}', '\u{2587}',
+        '\u{2588}',
+    ];
+
+    pub fn render(v: &Vec<i32>) -> Result<String, &'static str> {
+        if v.is_empty() {
+            return Err("Empty vector parameter");
+        }
+        let max = v.iter().max().unwrap();
+        let min = v.iter().min().unwrap();
+        let scale = (max - min) as f32 / 7.;
+        let mut s = String::new();
+        for e in v.iter() {
+            let i = (e - min) / scale as i32;
+            s.push_str(&Ticks[i as usize].to_string());
+        }
+        Ok(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_render() {
+        let v = vec![2, 250, 670, 890, 2, 430, 11, 908, 123, 57];
+        let res = rspark::render(&v).unwrap();
+        println!("{}", res);
+        assert_eq!("▁▂▆▇▁▄▁█▁▁", res);
+    }
+}
